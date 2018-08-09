e7d59a90cbb49ad096f201facefa8ef1b98866e4
Benjamin French <benjamin@bnjamin.com>
2018-08-09 12:14:55 +0200

    Rack::Utils.bytesize has been deprecated in rack 2.0. Ruby implements String#bytesize, which is also what rack called.

--- lib/rack_dav/controller.rb
+++ lib/rack_dav/controller.rb
@@ -399,7 +399,7 @@ module RackDAV
         end.to_xml
         response.body = [content]
         response["Content-Type"] = 'text/xml; charset=utf-8'
-        response["Content-Length"] = Rack::Utils.bytesize(content).to_s
+        response["Content-Length"] = content.bytesize.to_s
       end
 
       def multistatus
